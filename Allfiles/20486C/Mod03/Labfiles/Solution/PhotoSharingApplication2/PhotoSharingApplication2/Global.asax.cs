b63332d3b52d868564582ad81273d5b54e81230c
Rob Foulkrod <rob.foulkrod@nhls.com>
2018-03-04 22:47:11 -0500

    Finished the solution for module 3

--- Allfiles/20486C/Mod03/Labfiles/Solution/PhotoSharingApplication2/PhotoSharingApplication2/Global.asax.cs
+++ Allfiles/20486C/Mod03/Labfiles/Solution/PhotoSharingApplication2/PhotoSharingApplication2/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using PhotoSharingApplication2.Models;
 
 namespace PhotoSharingApplication2
 {
@@ -16,6 +18,7 @@ namespace PhotoSharingApplication2
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<PhotoSharingContext>(new PhotoSharingInitializer());
         }
     }
 }
